411177a9f53f34e90632b9fc72f0e6432b2f9118
nablum <hugo.besnard.pro@gmail.com>
2025-06-14 16:21:43 +0200

    license update

--- firmware/effects/MultiEffects/MultiEffects.ino
+++ firmware/effects/MultiEffects/MultiEffects.ino
@@ -1,5 +1,30 @@
-// Licensed under a Creative Commons Attribution 4.0 International License.
-/* MultiEffects.ino allows selection between delay, echo, distortion, fuzz, bit-crusher, octaver, octave crusher, vibrato and chorus effects. */
+/*
+  MultiEffects.ino allows selection between delay, echo, distortion, fuzz, 
+  bit-crusher, octaver, octave crusher, vibrato and chorus effects.
+
+  Part of the pedalSHIELD-MEGA project: https://github.com/nablum/pedalSHIELD-MEGA
+
+  Copyright (c) 2025 Nablum
+  SPDX-License-Identifier: MIT
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
 
 //display parameters
 #include "U8glib.h"
